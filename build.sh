6786eecae12241dfe7ff8b96b64a2cd7c062c44b
dosse91 <info@fdossena.com>
2021-07-15 20:40:36 +0200

    Migliorato build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 xelatex -interaction=nonstopmode Main.tex
-if [ $? -eq 0 ]; then
-    gs -dPDFA=1 -dBATCH -dNOPAUSE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile=Main.A.pdf Main.pdf
-else
-    echo Build failed
-fi;
+bibtex Main
+xelatex -interaction=nonstopmode Main.tex
+xelatex -interaction=nonstopmode Main.tex
+gs -dPDFA=1 -dBATCH -dNOPAUSE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile=Main.A.pdf Main.pdf
