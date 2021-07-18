821dd792001c59161812bbc8c9d929b6e564a530
dosse91 <info@fdossena.com>
2021-07-18 15:44:39 +0200

    Aggiunto placeholder presentazione

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ bibtex Main
 xelatex -interaction=nonstopmode Main.tex
 xelatex -interaction=nonstopmode Main.tex
 gs -dPDFA=1 -dBATCH -dNOPAUSE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile=Main.A.pdf Main.pdf
+xelatex -interaction=nonstopmode Presentazione.tex
+xelatex -interaction=nonstopmode Presentazione.tex
+xelatex -interaction=nonstopmode Presentazione.tex
