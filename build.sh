a843e5bda963d5ff9d87205dcb2de52930b96640
dosse91 <info@fdossena.com>
2021-07-24 13:36:09 +0200

    Aggiunte altre slide e aggiornato build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 buildTex () {
+    echo "$1: passo 1"
     xelatex -interaction=nonstopmode "$1.tex" >/dev/null 2>&1
+    echo "$1: bibtex"
     bibtex $1 >/dev/null 2>&1
+    echo "$1: passo 2"
     xelatex -interaction=nonstopmode "$1.tex" >/dev/null 2>&1
+    echo "$1: passo 3"
     xelatex -interaction=nonstopmode "$1.tex" >/dev/null 2>&1
+    echo "$1: compilato"
 }
 pdfA(){
+    echo "$1: generazione PDF/A"
     gs -dPDFA=1 -dBATCH -dNOPAUSE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile="$1.A.pdf" "$1.pdf" >/dev/null 2>&1
+    echo "$1: PDF/A generato"
 }
 
 echo "Compilazione parallela avviata"
-echo "L'output è nascosto, ma i file di log vengono generati"
+echo "L'output è nascosto, ma vengono generati i file di log"
 {
     buildTex Main
-    echo "Tesi: compilata"
     pdfA Main
-    echo "Tesi: PDF/A generato"
+    echo "Main: Fine"
 } &
 {
     buildTex Presentazione
-    echo "Presentazione: compilata"
+    echo "Presentazione: Fine"
 } &
 wait
 echo "Tutto finito"
