d27a72a3a66773a66d2cc8bbd8792f3896e4fa82
dosse91 <info@fdossena.com>
2021-07-24 09:43:33 +0200

    Aggiunte nuove slide alla presentazione

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ xelatex -interaction=nonstopmode Main.tex
 xelatex -interaction=nonstopmode Main.tex
 gs -dPDFA=1 -dBATCH -dNOPAUSE -sProcessColorModel=DeviceRGB -sDEVICE=pdfwrite -sPDFACompatibilityPolicy=1 -sOutputFile=Main.A.pdf Main.pdf
 xelatex -interaction=nonstopmode Presentazione.tex
+bibtex Presentazione
 xelatex -interaction=nonstopmode Presentazione.tex
 xelatex -interaction=nonstopmode Presentazione.tex
